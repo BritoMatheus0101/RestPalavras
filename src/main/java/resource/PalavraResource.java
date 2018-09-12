a671813ab95c3abbc46d67e6b01321a336409dc7
BritoMatheus0101 <36920577+BritoMatheus0101@users.noreply.github.com>
2018-09-12 12:17:41 -0300

    Criação do controller com post e get -- TESTE

---
+++ src/main/java/resource/PalavraResource.java
@@ -0,0 +1,32 @@
+package resource;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import models.Palavra;
+import repository.Palavras;
+
+@RestController
+@RequestMapping("/palavras")
+public class PalavraResource {
+	@Autowired
+	private Palavras palavras;
+	
+	@PostMapping 
+	public Palavra adicionar(@Valid @RequestBody Palavra palavra) {
+		return palavras.save(palavra);
+	}
+	
+	@GetMapping
+	public List<Palavra> listar(){
+		return palavras.findAll();
+	}
+}
